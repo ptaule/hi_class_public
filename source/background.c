5cc8477434fc28ec0ca75734653b9363aaf6ef0a
Petter Taule <pettertaule@live.no>
2025-01-17 14:19:28 +0100

    Fix alphaK computation

--- source/background.c
+++ source/background.c
@@ -3787,17 +3787,17 @@ int background_gravity_functions(
 
       /* Change to hi_class convention */
       double bra = -2 * c_b* pow(a, eta);
-      double bra_p = -2 * c_b * eta * pow(a, eta-1);
       double run = c_m*pow(a, eta);
       double ten = c_t*pow(a, eta);
       double M2 = exp(c_m * pow(a, eta)/eta);
       double dM2 = M2 - 1;
 
       double H = pvecback[pba->index_bg_H];
+      double rho_m = pvecback[pba->index_bg_rho_tot_wo_smg];
       double rho_smg = pvecback[pba->index_bg_rho_smg];
-      double rho_tot = pvecback[pba->index_bg_rho_tot_wo_smg];
       double p_smg = pvecback[pba->index_bg_p_smg];
-      double p_tot = pvecback[pba->index_bg_p_tot_wo_smg];
+
+      double bra_p = -2 * c_b * eta * pow(a, eta+1) * H;
 
       pvecback[pba->index_bg_braiding_smg] = bra;
       pvecback[pba->index_bg_mpl_running_smg] = run;
@@ -3805,12 +3805,25 @@ int background_gravity_functions(
       pvecback[pba->index_bg_M2_smg] = M2;
       pvecback[pba->index_bg_delta_M2_smg] = dM2;
 
-      double lambda_2 = (- 2.*dM2 + bra*M2)*(rho_tot + p_tot)*(-3.)/2.*pow(H,-2)*pow(M2,-1) + ((-2.) + bra)*(rho_smg + p_smg)*(-3.)/2.*pow(H,-2) + a * bra_p;
+      double lambda_2 = 0;
+      if (pba->expansion_model_smg == evolve_Mp) {
+        lambda_2 = pow(H,-2)*pow(M2,-1) * ( (1 - bra/2.) * (rho_m + rho_smg + p_smg)*(3.) - rho_m*(3.)) + pow(H,-1)*bra_p*pow(a,-1);
+      }
+      else {
+        lambda_2 = (- 2.*dM2 + bra*M2)*(rho_tot + p_tot)*(-3.)/2.*pow(H,-2)*pow(M2,-1) + ((-2.) + bra)*(rho_smg + p_smg)*(-3.)/2.*pow(H,-2) + pow(H,-1)*bra_p*pow(a,-1);
+      }
+
       double cs2num = ((-2.) + bra)*((-1.)*bra + (-2.)*run + 2.*ten + (-1.)*bra*ten)*1./2. + lambda_2;
 
       double kin = cs2num - 3./2.*pow(bra,2);
-      if (kin < 1e-25) {
-        kin = 1e-25;
+
+      if (fabs(kin) < 1e-25) {
+        if (kin < 0) {
+          kin = -1e-25;
+        }
+        else{
+          kin = 1e-25;
+        }
       }
       pvecback[pba->index_bg_kineticity_smg] = kin;
     }
